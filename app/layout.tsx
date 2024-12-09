1823e1a0fd356b861016e843751311ac0b84a30c
Anonymous <vissar2016@gmail.com>
2024-12-09 18:05:15 +0300

    feat: fonts added, cn lib added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../src/app/styles/global.css";
+import "../src/app/fonts/fonts.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, aspernatur?!</h1>
+    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos tempore totam consectetur porro magni consequatur harum in repellat quasi, laudantium, magnam omnis?</h2>
   </StrictMode>
 );
