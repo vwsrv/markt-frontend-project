a1becc97576dd3b5021300787125f90cb8894789
Anonymous <vissar2016@gmail.com>
2024-12-09 16:23:29 +0300

    feat: husky added, project init

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../src/app/styles/global.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+  </StrictMode>
+);
