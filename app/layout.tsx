1efce21ba84e4d30827f64f758da1c0a0c52842d
Anonymous <vissar2016@gmail.com>
2024-12-10 12:45:31 +0300

    feat: main-btn component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../src/app/styles/global.css";
-import "../src/app/fonts/fonts.css";
+import { Button } from "../src/shared/btn-main";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, aspernatur?!</h1>
-    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos tempore totam consectetur porro magni consequatur harum in repellat quasi, laudantium, magnam omnis?</h2>
+    <Button>В корзину</Button>
   </StrictMode>
 );
