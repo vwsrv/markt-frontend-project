277a8187bab8ae429f22968ff63774aef26436b4
Anonymous <vissar2016@gmail.com>
2024-12-12 17:32:05 +0300

    refactor: image-background component refactored

--- src/shared/ui/image-background/types.ts
+++ src/shared/ui/image-background/types.ts
@@ -1,6 +1,12 @@
 export type typeImageBackgroundProps = {
+  images: Array<{
+    id: number;
+    src: string;
+    name?: string;
+  }>;
   handler: ((id: number) => void) | (() => void) | string;
   variant?: "red" | "yellow" | "blue" | "grey" | "blueGradient";
   saleName?: string;
   saleValue?: string;
+  layout?: "one" | "two" | "ten" | "four" | "wideAndShort";
 };
