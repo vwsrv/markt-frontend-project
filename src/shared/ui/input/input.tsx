3e0af0dbf68439562a371b7f078881b16f74c875
Bl Bltich <vissar2016@yandex.ru>
2025-01-23 02:59:28 +0300

    refactor: compiler fix

--- src/shared/ui/input/input.tsx
+++ src/shared/ui/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef } from "react";
+import { forwardRef } from "react";
 import classes from "./styles.module.scss";
 import cn from "classnames";
 import { InputProps } from "./types";
