bfa8b9abb0792f41a13aa053c87da0e92fe558a7
Bl Bltich <vissar2016@yandex.ru>
2025-01-28 03:12:59 +0300

    refactor: pr-crd component refactored

--- src/shared/ui/product-card/productCard.tsx
+++ src/shared/ui/product-card/productCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { ProductImage } from "../product-image";
 import { ProductInfo } from "../product-info";
@@ -6,15 +8,11 @@ import cn from "classnames";
 import classes from "./styles.module.scss";
 
 export const ProductCard: React.FC<typeProductCardProps> = (props) => {
-  const { saleName, saleValue, productData, type } = props;
+  const { productData, type } = props;
 
   return (
     <article className={cn(classes.card)}>
-      <ProductImage
-        saleName={saleName}
-        saleValue={saleValue}
-        productData={[productData]}
-      />
+      <ProductImage productData={[productData]} />
       <ProductInfo productData={[productData]} type={type} />
     </article>
   );
--- src/shared/ui/product-card/types.ts
+++ src/shared/ui/product-card/types.ts
@@ -2,8 +2,6 @@ import { typeProductInfoProps } from "../product-info/types";
 import { BaseProductProps } from "../../../types/productTypes";
 
 export type typeProductCardProps = {
-  saleName: string;
-  saleValue: string;
   type: typeProductInfoProps["type"];
   productData: BaseProductProps;
 };
